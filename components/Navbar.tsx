d50d065cfea2f3563c3627eda5ab9fe5e5c82e48
Ethan Burkett <ethan@ethanburkett.net>
2022-04-16 01:52:17 -0500

    Initial commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,31 @@
+export default function Navbar(): JSX.Element {
+  return (
+    <>
+      <div className="w-screen h-20 fixed top-0 left-0 flex flex-row justify-start items-center">
+        <div className="w-screen h-20 flex flex-row items-center justify-start p-16">
+          <p className="text-5xl">LOGO</p>
+        </div>
+        <div className="w-screen h-20 flex flex-row items-center justify-evenly p-16 font-['Raleway']">
+          <li
+            className="text-white text-xl font-light cursor-pointer"
+            key={"about"}
+          >
+            About Me
+          </li>
+          <li className="text-white text-xl cursor-pointer" key={"skills"}>
+            Skills
+          </li>
+          <li className="text-white text-xl cursor-pointer" key={"portfolio"}>
+            Portfolio
+          </li>
+          <li
+            className="text-black bg-white pl-3 cursor-pointer transition-all pr-3 pt-2 pb-2 rounded-full text-xl font-extrabold "
+            key={"contact"}
+          >
+            Contact Me
+          </li>
+        </div>
+      </div>
+    </>
+  );
+}
